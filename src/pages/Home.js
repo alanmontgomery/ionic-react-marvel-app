e8b6176d8bf2e55fe5bcfd0bef8e0ad81c24a0a8
Alan Montgomery <alan.montgomery@tascomi.com>
2021-03-26 11:02:20 +0000

    Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonPage, IonRow, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
 import './Home.css';
-import { expandOutline, gridOutline, searchOutline } from 'ionicons/icons';
+import { addOutline, expandOutline, gridOutline, searchOutline } from 'ionicons/icons';
 import CharacterItem from '../components/CharacterItem';
 
 const Home = () => {
@@ -55,6 +55,38 @@ const Home = () => {
 		e.target.complete();
 	}
 
+	const addToHomeScreen = () => {
+
+		let deferredPrompt;
+		const addBtn = document.querySelector('.add-button');
+		addBtn.style.display = 'none';
+
+		window.addEventListener('beforeinstallprompt', (e) => {
+			// Prevent Chrome 67 and earlier from automatically showing the prompt
+			e.preventDefault();
+			// Stash the event so it can be triggered later.
+			deferredPrompt = e;
+			// Update UI to notify the user they can add to home screen
+			addBtn.style.display = 'block';
+		  
+			addBtn.addEventListener('click', (e) => {
+			  // hide our user interface that shows our A2HS button
+			  addBtn.style.display = 'none';
+			  // Show the prompt
+			  deferredPrompt.prompt();
+			  // Wait for the user to respond to the prompt
+			  deferredPrompt.userChoice.then((choiceResult) => {
+				  if (choiceResult.outcome === 'accepted') {
+					console.log('User accepted the A2HS prompt');
+				  } else {
+					console.log('User dismissed the A2HS prompt');
+				  }
+				  deferredPrompt = null;
+				});
+			});
+		  });
+	}
+
 	return (
 		<IonPage id="home-page">
 			<IonHeader>
@@ -63,6 +95,10 @@ const Home = () => {
 					<IonButtons slot="end">
 						<IonButton color="dark">
 							<IonIcon icon={ searchOutline } />
+						</IonButton>
+
+						<IonButton color="dark" className="add-button" onClick={ () => addToHomeScreen() }>
+							<IonIcon icon={ addOutline } />
 						</IonButton>
 
 						<IonButton color="danger" onClick={ () => setGrid(grid => !grid) }>
