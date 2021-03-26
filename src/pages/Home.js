cda5d546325d0d93baf3f07e42f32f7eb3111cd4
Alan Montgomery <alan.montgomery@tascomi.com>
2021-03-26 11:06:19 +0000

    Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,20 @@ import './Home.css';
 import { addOutline, expandOutline, gridOutline, searchOutline } from 'ionicons/icons';
 import CharacterItem from '../components/CharacterItem';
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
 const Home = () => {
 
 	const [ grid, setGrid ] = useState(true);
@@ -57,34 +71,16 @@ const Home = () => {
 
 	const addToHomeScreen = () => {
 
-		let deferredPrompt;
-		const addBtn = document.querySelector('.add-button');
-		addBtn.style.display = 'none';
-
-		window.addEventListener('beforeinstallprompt', (e) => {
-			// Prevent Chrome 67 and earlier from automatically showing the prompt
-			e.preventDefault();
-			// Stash the event so it can be triggered later.
-			deferredPrompt = e;
-			// Update UI to notify the user they can add to home screen
-			addBtn.style.display = 'block';
-		  
-			addBtn.addEventListener('click', (e) => {
-			  // hide our user interface that shows our A2HS button
-			  addBtn.style.display = 'none';
-			  // Show the prompt
-			  deferredPrompt.prompt();
-			  // Wait for the user to respond to the prompt
-			  deferredPrompt.userChoice.then((choiceResult) => {
-				  if (choiceResult.outcome === 'accepted') {
-					console.log('User accepted the A2HS prompt');
-				  } else {
-					console.log('User dismissed the A2HS prompt');
-				  }
-				  deferredPrompt = null;
-				});
-			});
-		  });
+		// Hide the app provided install promotion
+		hideInstallPromotion();
+		// Show the install prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		const { outcome } = await deferredPrompt.userChoice;
+		// Optionally, send analytics event with outcome of user choice
+		console.log(`User response to the install prompt: ${outcome}`);
+		// We've used the prompt, and can't use it again, throw it away
+		deferredPrompt = null;
 	}
 
 	return (
